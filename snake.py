8348cd06218e50b1c5f03ba308d52ce7cd546eed
F0lha <up201305337@fe.up.pt>
2017-07-06 08:31:45 +0100

    bug fix

--- snake.py
+++ snake.py
@@ -19,6 +19,7 @@ fruit = list(snakeHead)
 
 def randomFruit(snake):
     global fruit
+    fruit = [random.randrange(8),random.randrange(8)]
     while fruit in snake:
         fruit = [random.randrange(8),random.randrange(8)]
 
@@ -31,6 +32,7 @@ direction = 1
         
 def moveSnake():
     global fruit, snake, direction
+    removed = None
     head = list(snake[0])
     if(direction == 0):
         head[0] = (head[0] + 1) % 8
@@ -40,6 +42,13 @@ def moveSnake():
         head[0] = (head[0] - 1) % 8
     elif(direction == 3):
         head[1] = (head[1] + 1) % 8
+
+    #if eats fruit
+    if head != fruit:
+        removed = snake.pop(-1)
+    else:
+        randomFruit(snake)
+        
     if head in snake:
         sense.set_pixel(head[0],head[1],255,0,0)
         finalScreen = sense.get_pixels()
@@ -53,26 +62,29 @@ def moveSnake():
         sys.exit()
     else:
         snake = [head] + snake
-        if head != fruit:
-            return snake.pop(-1)
-        else:
-            randomFruit(snake)
+    return removed
+
 
 def drawWorld(removed):
     global fruit, snake
-    
+    first = True
     if removed is not None:
         sense.set_pixel(removed[0],removed[1],0,0,0)
     for point in snake:
-        sense.set_pixel(point[0],point[1],255,255,255)
+        if first:
+            sense.set_pixel(point[0],point[1],255,255,0)
+            first = False
+        else: sense.set_pixel(point[0],point[1],255,255,255)
     sense.set_pixel(fruit[0],fruit[1],0,255,0)
 
 def updateWorld():
     removed = moveSnake()
-    drawWorld( removed)
+    drawWorld(removed)
     pygame.time.set_timer(updateWorldID, 250)
         
 pygame.time.set_timer(updateWorldID, 250)
+
+sense.set_rotation(180)
 
 while 1:
     for event in pygame.event.get():
